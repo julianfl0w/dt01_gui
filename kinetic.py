b787f5f65e9dc2b3307363232669ee5b7cc2a3f9
Julian <dragonflysynths@gmail.com>
2021-01-30 04:12:06 -0500

    arbitrary image button array

--- kinetic.py
+++ kinetic.py
@@ -4,34 +4,84 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import * 
 from text_to_image import *
 
+allpatches = dict()
+last_released = 0
+
 class MainWindow(QWidget):
+
+	global allpatches
+	def showReleased(self):
+		global allpatches
+		sender = self.sender()
+		my_button, colorTuple, x = allpatches[id(sender)]
+		
+		sender.setIcon(QIcon(colorTuple[2]))
+		sender.setIconSize(QSize(200, 70))
+		
+		global last_released
+		allpatches[id(my_button)] = (my_button, colorTuple, 2)
+		# apply patch now
+		last_released = id(sender)
+		
+	def showPressed(self):
+		global last_released
+		sender = self.sender()
+		my_button, colorTuple, x = allpatches[id(sender)]
+		sender.setIcon(QIcon(colorTuple[1]))
+		sender.setIconSize(QSize(200, 70))
+		allpatches[id(sender)] = (sender, colorTuple, 1)
+		
+		# apply patch now
+		try: 
+			my_button, colorTuple, x = allpatches[last_released]
+			my_button.setIcon(QIcon(colorTuple[0]))
+			x = 0
+			allpatches[id(my_button)] = (my_button, colorTuple, x)
+		except:
+			pass
+		
+		#for key, value in allpatches.items():
+		#	my_button, colorTuple, x = value
+		#	if id(my_button) == id(sender):
+		#		continue
+		#	if x != 0:
+		#		my_button.setIcon(QIcon(colorTuple[0]))
+		#		x = 0
+		#		allpatches[id(my_button)] = (my_button, colorTuple, x)
+		
 	def __init__(self, parent=None):
 		super().__init__(parent)
 		scroll_area = QScrollArea()
-		scroll_area.setMinimumWidth(3000)
+		#scroll_area.setMinimumWidth(3000)
 		layout = QGridLayout(self)
 		layout.addWidget(scroll_area)
 
 		scroll_widget = QWidget()
 		scroll_layout = QFormLayout(scroll_widget)
-
-		for i in range(50):
 		
-			buttonLabel = 'Label #{}'.format(i)
+		global allpatches
+
+		for i in range(30):
+		
+			buttonLabel = 'Label{}'.format(i)
 			labelImage = text_to_image(buttonLabel)
-			_icon = QIcon(labelImage)
+			_icon = QIcon(labelImage.replace("__COLOR__", '0'))
 			my_button = QPushButton()
 			my_button.setIcon(_icon)
-			my_button.setIconSize(QSize(720, 70))
+			my_button.setIconSize(QSize(200, 70))
+			my_button.pressed.connect(self.showPressed) 
+			my_button.released.connect(self.showReleased) 
 			scroll_layout.addRow(my_button)
+			colorTuple = (labelImage.replace("__COLOR__", '0'), labelImage.replace("__COLOR__", '1'), labelImage.replace("__COLOR__", '2'))
+			allpatches[id(my_button)] = [my_button, colorTuple, 0]
 
 		scroll_area.setWidget(scroll_widget)
 
 		QScroller.grabGesture(
 			scroll_area.viewport(), QScroller.LeftMouseButtonGesture
 		)
-
-
+		
+		
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
 	main_window = MainWindow()
--- text_to_image.py
+++ text_to_image.py
@@ -12,31 +12,33 @@ regex.sub('', 'ab3d*E')
 #Out: 'abdE'
 
 def text_to_image(txt):
-	# W, H = (1280, 720) # image size
-	W, H = (720, 70) # image size
-	background = (0,164,201) # white
-	fontsize = 35
-	font = ImageFont.truetype(pacifico, fontsize)
-
-	image = Image.new('RGBA', (W, H), background)
-	draw = ImageDraw.Draw(image)
-
-	# w, h = draw.textsize(txt) # not that accurate in getting font size
-	w, h = font.getsize(txt)
-
-	draw.text((10,(H-h)/2), txt, fill='white', font=font)
-	# draw.text((10, 0), txt, (0,0,0), font=font)
-	# img_resized = image.resize((188,45), Image.ANTIALIAS)
-	
-	
 	regex = re.compile('[^a-zA-Z0-9]')
 	#First parameter is the replacement, second parameter is your input string
 	fileFriendly = regex.sub('', txt)
-
-	# img_resized.save(save_location + '/sample.jpg')
-	outName = os.path.join(sys.path[0], 'patches', fileFriendly + '.png')
-	image.save(outName)
+		
+	colors = [(0,164,201), (164,201,0), (201,164,0)]
 	
+	outName = os.path.join(sys.path[0], 'patches', fileFriendly + '__COLOR__.png')
+	for i, color in enumerate(colors):
+		# W, H = (1280, 720) # image size
+		W, H = (200, 70) # image size
+		background = color # white
+		fontsize = 35
+		font = ImageFont.truetype(pacifico, fontsize)
+
+		image = Image.new('RGBA', (W, H), background)
+		draw = ImageDraw.Draw(image)
+
+		# w, h = draw.textsize(txt) # not that accurate in getting font size
+		w, h = font.getsize(txt)
+
+		draw.text((10,(H-h)/2), txt, fill='white', font=font)
+		# draw.text((10, 0), txt, (0,0,0), font=font)
+		# img_resized = image.resize((188,45), Image.ANTIALIAS)
+		
+		# img_resized.save(save_location + '/sample.jpg')
+		image.save(outName.replace("__COLOR__", str(i) ))
+		
 	return outName
 	
 if __name__ == "__main__":
