5286a077208417b55ef9d132b38e5cf9de3b87a0
Julian <jcloiacon@gmail.com>
2021-07-09 03:40:41 +0100

    SPI was just too fast to measure

--- spitest.py
+++ spitest.py
@@ -2,7 +2,11 @@ import spidev
 spi = spidev.SpiDev()
 
 spi.open(1, 0)
-to_send = [0x01, 0x02, 0x03]
+
+speed = 20000000
+spi.max_speed_hz=speed
+
 while(1):
-	spi.xfer(to_send)
-	spi.readbytes(100)
+    to_send = [0x01, 0x02, 0x03]
+    spi.xfer2(to_send)
+    #spi.readbytes(100)
--- startscript.sh
+++ startscript.sh
@@ -1,2 +1,5 @@
+# run sudo cp dt01.desktop /home/pi/.config/autostart/
+# then 
+# run chmod +x /home/pi/.config/autostart/*
 export DISPLAY=:0
-python3 /home/pi/dt01/kinetic.py &
+python3 /home/pi/dt01_gui/kinetic.py &
