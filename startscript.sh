2b18458881db45ce40496b22a4dae4e80b502272
Julian <jcloiacon@gmail.com>
2021-10-24 18:22:57 +0100

    reversion?

--- startscript.sh
+++ startscript.sh
@@ -1,6 +1,17 @@
+#!/bin/bash
 # run sudo cp dt01.desktop /home/pi/.config/autostart/
 # then 
 # run chmod +x /home/pi/.config/autostart/*
+
 export DISPLAY=:0
+
+#parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+#cd "$parent_path"
+if [[ -z ${STARTED+x} ]]
+then
+	echo "BIOTCH"
+	#sudo taskset 0x00000004 sudo python3 /home/pi/dt01_gui/startup.py &
+fi
+export STARTED=1
 python3 /home/pi/dt01_gui/kinetic.py &
-python3 /home/pi/dt01_gui/startup.py &
+#sudo taskset 0x00000004 sudo python3 startup.py &
--- unitTest.py
+++ unitTest.py
@@ -1,6 +1,7 @@
-from dt01 import *
+import dt01
 import sys
 import logging
+import os
 
 logger = logging.getLogger('DT01')
 formatter = logging.Formatter('{%(pathname)s:%(lineno)d %(message)s}')
@@ -15,11 +16,11 @@ filename = "dt01_" + str(int(polyphony)) + ".pkl"
 
 if os.path.exists(filename):
 	logger.debug("reading from file")
-	dt01_inst = DT01_fromFile(filename)
+	dt01_inst = dt01.DT01_fromFile(filename)
 	logger.debug("finished reading")
 else:
 	logger.debug("initializing from scratch")
-	dt01_inst = DT01(polyphony = polyphony)
+	dt01_inst = dt01.DT01(polyphony = polyphony)
 	logger.debug("saving to file")
 	dt01_inst.toFile(filename)
 	
@@ -32,38 +33,42 @@ logger.debug("Applying a light touch")
 # implement actual envelopes
 
 if "t" in sys.argv:
-	dt01_inst.voices[0].operators[6].send("cmd_env", 2**14)
-	dt01_inst.voices[0].operators[0].send("cmd_env", 2**16)
+	dt01_inst.voices[0].operators[6].send(dt01.cmd_env, 2**14)
+	dt01_inst.voices[0].operators[6].send(dt01.cmd_increment, 2**11)
+	dt01_inst.voices[0].operators[0].send(dt01.cmd_env, 2**16)
+	dt01_inst.voices[0].operators[0].send(dt01.cmd_increment, 2**23)
 	
 if "v" in sys.argv:
-	dt01_inst.voices[0].send("cmd_baseincrement", 2**14)
-	dt01_inst.voices[0].operators[0].send("cmd_env", 2**16)
-	dt01_inst.voices[0].operators[7].send("cmd_env", 2**7)
-	dt01_inst.voices[0].operators[7].send("cmd_increment", 2**11)
+	dt01_inst.voices[0].send(dt01.cmd_increment, 2**27)
+	dt01_inst.voices[0].operators[0].send(dt01.cmd_env, 2**16)
+	dt01_inst.voices[0].operators[7].send(dt01.cmd_env, 2**7)
 	
 if "fb" in sys.argv:
-	dt01_inst.voices[0].send("cmd_baseincrement", 2**13)
-	dt01_inst.voices[0].send("cmd_fm_algo", 0o77777770)
-	dt01_inst.voices[0].send("cmd_fbsrc", 0)
-	dt01_inst.voices[0].send("cmd_fbgain", 2**5)
-	dt01_inst.voices[0].operators[0].send("cmd_env", 2**16)
+	dt01_inst.voices[0].send(dt01.cmd_increment, 2**27)
+	dt01_inst.voices[0].send(dt01.cmd_fm_algo, 0o77777770)
+	dt01_inst.voices[0].send(dt01.cmd_fbsrc, 0)
+	dt01_inst.voices[0].send(dt01.cmd_fbgain, 2**5)
+	dt01_inst.voices[0].operators[0].send(dt01.cmd_env, 2**16)
 
+if "test" in sys.argv:
+	for i in range(1024):
+		dt01_inst.voices[0].operators[0].send(dt01.cmd_env, 2**16)
 
 if "f" in sys.argv:
-	dt01_inst.voices[0].send("cmd_baseincrement", 2**15)
-	dt01_inst.voices[0].operators[0].send("cmd_env", 2**16)
-	dt01_inst.voices[0].operators[1].send("cmd_env", 2**12)
-	dt01_inst.voices[0].operators[2].send("cmd_env", 2**12)
-	dt01_inst.voices[0].operators[3].send("cmd_env", 2**12)
-	dt01_inst.voices[0].operators[4].send("cmd_env", 2**7)
-	dt01_inst.voices[0].operators[5].send("cmd_env", 2**14)
-	dt01_inst.voices[0].operators[6].send("cmd_env", 2**16)
-	dt01_inst.voices[0].send("cmd_fm_algo", 0o77754321)
+	dt01_inst.voices[0].send(dt01.cmd_increment,2**27)
+	dt01_inst.voices[0].operators[0].send(dt01.cmd_env, 2**16)
+	dt01_inst.voices[0].operators[1].send(dt01.cmd_env, 2**12)
+	dt01_inst.voices[0].operators[2].send(dt01.cmd_env, 2**12)
+	dt01_inst.voices[0].operators[3].send(dt01.cmd_env, 2**12)
+	dt01_inst.voices[0].operators[4].send(dt01.cmd_env, 2**7)
+	dt01_inst.voices[0].operators[5].send(dt01.cmd_env, 2**14)
+	dt01_inst.voices[0].operators[6].send(dt01.cmd_env, 2**16)
+	dt01_inst.voices[0].send(dt01.cmd_fm_algo, 0o77754321)
 	
 if "p" in sys.argv:
 	dt01_inst.fpga_interface_inst.gather()
-	dt01_inst.voices[0].operators[0].send("cmd_env", 2**16)
-	dt01_inst.voices[1].operators[0].send("cmd_env", 2**16)
-	dt01_inst.voices[0].send("cmd_baseincrement", 2**13)
-	dt01_inst.voices[1].send("cmd_baseincrement", 2**13*3/2)
+	dt01_inst.voices[0].operators[0].send(dt01.cmd_env, 2**16)
+	dt01_inst.voices[1].operators[0].send(dt01.cmd_env, 2**16)
+	dt01_inst.voices[0].send(dt01.cmd_increment, 2**13)
+	dt01_inst.voices[1].send(dt01.cmd_increment, 2**13*3/2)
 	dt01_inst.fpga_interface_inst.release()
