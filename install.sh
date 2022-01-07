b67e10bc3c3fd683323cca23447090f1d5fc5e01
Julian <jcloiacon@gmail.com>
2022-01-07 00:27:11 +0000

    more startup

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ cd /home/pi/Downloads
 
 # setup screen
 DIR="/home/pi/Downloads/LCD-show"
-if ![ -d "$DIR" ]; then
+if [ ! -d "$DIR" ]; then
   # Take action if $DIR not exists. #
   echo "Setting up touchscreen..."
   sudo rm -rf LCD-show
@@ -13,17 +13,21 @@ if ![ -d "$DIR" ]; then
 fi
 cd ..
 
-if ! command -v conda &> /dev/null then
+if [ ! command -v conda &> /dev/null ]; then
   # install anaconda
   sudo apt-get update
+  sudo apt-get upgrade
   curl "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-armv7l.sh" -o "Miniconda.sh"
   bash ./Miniconda.sh
+  # only do these copies here. boot info for RPI
+  sudo cp $DIR/random/cmdline.txt /boot/cmdline.txt
+  sudo cp $DIR/random/config.txt /boot/config.txt
 fi
 
 cd ..
 
 DIR="/home/pi/dt01_gui"
-if ![ -d "$DIR" ]; then
+if [ ! -d "$DIR" ]; then
   # Take action if $DIR not exists. #
   echo "Downloading synth code..."
   git clone https://github.com/julianfl0w/dt01_gui
@@ -33,5 +37,4 @@ if ![ -d "$DIR" ]; then
   conda install --file requirements.txt
 fi
 
-sudo cp random/cmdline.txt /boot/cmdline.txt
-sudo cp random/config.txt /boot/config.txt
+pwd
